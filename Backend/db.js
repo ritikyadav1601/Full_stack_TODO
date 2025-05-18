26ade97bdf1f41acc0ab2ffc3ef2d5a9f5899366
ritikyadav1601 <124035362+ritikyadav1601@users.noreply.github.com>
2025-05-18 22:25:46 +0530

    Update db.js
    Personal monodb address changed

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const db = mongoose.connect("mongodb+srv://rythukran713:hoWUzqby6cTkG7D6@cluster0.1mdayd4.mongodb.net/todo-app")
+const db = mongoose.connect("mongodb+srv://rythukran713:hoWUzqG7D6@cluster0.1mdayd4.mongodb.net/todo-app")
 
 const schema = new mongoose.Schema({
     title: String,
